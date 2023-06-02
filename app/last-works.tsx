cea9908527e9a8264915c2f23511f4dc766afb82
elysee15 <elyseebleu1@gmail.com>
2023-06-02 10:11:26 +0000

    fix last work section

--- app/last-works.tsx
+++ app/last-works.tsx
@@ -5,64 +5,76 @@ function LastWorks() {
   return (
     <section className="container md:px-20">
       <h2 className="text-xl font-semibold mb-10 text-center">Last work</h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-x-4 gap-y-5 md:gap-y-8">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-x-8 gap-y-20 md:gap-y-20">
         <div className="relative h-96 w-full aspect-square">
-          <Image
-            src="/project_1.svg"
-            alt="Project 1"
-            className="object-cover"
-            fill
-          />
+          <div className="relative h-full w-full">
+            <Image
+              src="/project_1.svg"
+              alt="Project 1"
+              className="object-cover"
+              fill
+            />
+          </div>
           <h3 className="text-sm my-2 font-semibold">Project title</h3>
           <p className="text-[#2D2D2D] text-xs">UI, Art drection</p>
         </div>
         <div className="relative h-96 w-full aspect-square">
-          <Image
-            src="/project_2.svg"
-            alt="Project 2"
-            fill
-            className="object-cover"
-          />
+          <div className="relative h-full w-full">
+            <Image
+              src="/project_2.svg"
+              alt="Project 2"
+              fill
+              className="object-cover"
+            />
+          </div>
           <h3 className="text-sm my-2 font-semibold">Project title</h3>
           <p className="text-[#2D2D2D] text-xs">UI, Art drection</p>
         </div>
         <div className="relative h-96 w-full aspect-square">
-          <Image
-            src="/project_3.svg"
-            alt="Project 3"
-            className="object-cover"
-            fill
-          />
+          <div className="relative h-full w-full">
+            <Image
+              src="/project_3.svg"
+              alt="Project 3"
+              className="object-cover"
+              fill
+            />
+          </div>
           <h3 className="text-sm my-2 font-semibold">Project title</h3>
           <p className="text-[#2D2D2D] text-xs">UI, Art drection</p>
         </div>
         <div className="relative h-96 w-full aspect-square">
-          <Image
-            src="/project_4.svg"
-            alt="Project 3"
-            className="object-cover"
-            fill
-          />
+          <div className="relative h-full w-full">
+            <Image
+              src="/project_4.svg"
+              alt="Project 3"
+              className="object-cover"
+              fill
+            />
+          </div>
           <h3 className="text-sm my-2 font-semibold">Project title</h3>
           <p className="text-[#2D2D2D] text-xs">UI, Art drection</p>
         </div>
         <div className="relative h-96 w-full aspect-square">
-          <Image
-            src="/project_5.svg"
-            alt="Project 3"
-            className="object-cover"
-            fill
-          />
+          <div className="relative h-full w-full">
+            <Image
+              src="/project_5.svg"
+              alt="Project 3"
+              className="object-cover"
+              fill
+            />
+          </div>
           <h3 className="text-sm my-2 font-semibold">Project title</h3>
           <p className="text-[#2D2D2D] text-xs">UI, Art drection</p>
         </div>
-        <div className="relative h-96 w-full aspect-square">
-          <Image
-            src="/project_6.svg"
-            alt="Project 3"
-            className="object-cover"
-            fill
-          />
+        <div className="h-96 w-full aspect-square">
+          <div className="relative h-full w-full">
+            <Image
+              src="/project_6.svg"
+              alt="Project 3"
+              className="object-cover"
+              fill
+            />
+          </div>
           <h3 className="text-sm my-2 font-semibold">Project title</h3>
           <p className="text-[#2D2D2D] text-xs">UI, Art drection</p>
         </div>
